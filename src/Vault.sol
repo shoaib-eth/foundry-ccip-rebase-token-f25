463e85bc3b0ecb6c51b5f4595da7e47a14106f4b
ShoaibDev69 <shoaib.developer69@gmail.com>
2025-05-06 08:54:14 +0000

    Vault Contract

---
+++ src/Vault.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IRebaseToken} from "./Interfaces/IRebaseToken.sol";
+
+contract Vault {
+    error Vault__TransferFailed();
+    // we need to pass the token address to the constructor
+    // create a deposit funciton that mints tokens from the user equal to the amount of ETH the user has sent
+    // create a redeem function that burns tokens from the user and sends the user ETH
+    // create a way to add rewards to the vault
+
+    IRebaseToken private immutable i_rebaseToken;
+
+    event Deposited(address indexed user, uint256 amount);
+    event Redeemed(address indexed user, uint256 amount);
+
+    constructor(IRebaseToken _rebaseToken) {
+        i_rebaseToken = _rebaseToken;
+    }
+
+    receive() external payable {}
+
+    /**
+     * @notice This function allows the user to deposit ETH into the vault and mint rebase tokens in return
+     */
+    function deposit() external payable {
+        // 1. we need to use the amount of ETH, the user has sent to mint the tokens to the user
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Allows users to redeem their rebase tokens for ETH
+     * @param _amount The amount of rebase tokens to redeem
+     */
+    function redeem(uint256 _amount) external {
+        // 1. burns the tokens from the user
+        i_rebaseToken.burn(msg.sender, _amount);
+        // 2. we need to send the user ETH
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__TransferFailed();
+        }
+        emit Redeemed(msg.sender, _amount);
+    }
+
+    /**
+     * @notice Get the address of the rebase token
+     * @return The address of the rebase token
+     */
+    function getRebaseTokenAddress() external view returns (address) {
+        return address(i_rebaseToken);
+    }
+}
